41a5ca539dc6272dbf1319880b62087d7b382ab6
이태균 <>
2022-07-05 23:57:48 +0900

    [이태균] -최초 파일 추가

---
+++ src/main/java/com/taegyun/test/SpringTestApplication.java
@@ -0,0 +1,16 @@
+package com.taegyun.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class SpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestApplication.class, args);
+	}
+
+}
